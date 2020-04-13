82416bcce4c6efe85a891779c18a7c5ae403e6f3
Marfan Fragoso <marfan@favotecnologia.com.br>
2020-04-12 21:10:54 -0300

    simplifying

--- lib/main.dart
+++ lib/main.dart
@@ -14,48 +14,19 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Crazy app'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String firstString = "Favo";
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
-        child: Text("Menu"),
-      ),
       appBar: AppBar(
-        title: Text(firstString),
+        title: Text("Poke App"),
+        backgroundColor: Colors.cyan,
       ),
-      body: Center(
-        child: ListView(
-          children: <Widget>[
-            ListTile(
-
-              title: Text("Pokemon"),
-              subtitle: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Icon(Icons.ac_unit, color: Colors.pink),
-                ],
-              ),
-              trailing: Text("bla bla bla"),
-            )
-          ],
-        ),
-      )
     );
   }
 }
